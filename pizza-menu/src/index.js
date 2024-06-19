acacae4d411d6bf4e6501a3ba77f8e608a612c4d
priyanshuahir000 <priyanshuahir01@gmail.com>
2024-06-19 16:17:43 +0530

    First commit

---
+++ pizza-menu/src/index.js
@@ -0,0 +1,108 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+  
+
+function App() {
+    return <div className="container">
+        <Header />
+        <Menu />
+        <Footer />
+    </div>;
+}
+
+function Header(){
+    return (
+        <header className="header">
+            <h1>Fast React Pizza Co.</h1>
+        </header>
+    )
+}
+
+function Menu(){
+    return (
+        <main className="menu">
+            <h2>Our Menu</h2>
+            <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.</p>
+            <ul className="pizzas">
+            {pizzaData.map((pizza, index)=>{return <Pizza key={index} {...pizza} />})}
+            </ul>
+        </main>
+    )
+}
+
+function Pizza(props){
+    return(<li className={`pizza ${props.soldOut && "sold-out"}`}>
+        <img src={props.photoName} alt={props.name} />
+        <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.soldOut ? "Sold Out":props.price}</span>        
+        </div>
+    </li>)
+}
+
+function Footer(){
+    return (
+        <footer>
+            <div className="order">
+            <p>We're happy to welcome you between 12:00 and 22:00.</p>
+            <button className="btn">Order</button>
+            </div>
+        </footer>
+    )
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
+
+export default App;
---
+++ travel-list/src/App.js
@@ -0,0 +1,67 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need for your 😍 trip</h3>
+      <select>
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+      </select>
+      <input type="text" placeholder="Item..." />
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item i={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ i }) {
+  return (
+    <li>
+      <span style={i.packed ? { textDecoration: "line-through" } : {}}>
+        {i.quantity} {i.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        💼 You have X item on your list, and you have already packed X (X%)
+      </em>
+    </footer>
+  );
+}
+export default App;

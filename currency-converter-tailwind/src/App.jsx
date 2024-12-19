d497f516ba6c50f338ae27c317bb115623d705e3
Priyanshu Baraiya <priyanshuahir01@gmail.com>
2024-12-19 12:09:24 +0530

    Add InputBox component and update App layout with background image

--- currency-converter-tailwind/src/App.jsx
+++ currency-converter-tailwind/src/App.jsx
@@ -3,6 +3,12 @@ import "./App.css";
 function App() {
   return (
     <>
+      <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        style={{ backgroundImage: `url("https://images.pexels.com/photos/210574/pexels-photo-210574.jpeg")` }}
+      >
+        <h1>Hello</h1>
+      </div>
     </>
   );
 }

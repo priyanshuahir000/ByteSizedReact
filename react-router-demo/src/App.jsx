b827de62639fd27105feb8e5d7cfb12bda805e21
Priyanshu Baraiya <priyanshuahir01@gmail.com>
2024-12-21 10:40:43 +0530

    Set up initial React project with Vite and Tailwind CSS, including basic app structure and configuration files

---
+++ react-router-demo/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-300">React Router</h1>
+    </>
+  );
+}
+
+export default App;
